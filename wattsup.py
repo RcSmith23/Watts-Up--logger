59d8d866764383ab10d36539442ce66f3d07138a
Ryan Smith <ryanc.smith@outlook.com>
2014-12-12 17:07:55 -0500

    Added logging method to wattsup

--- wattsup.py
+++ wattsup.py
@@ -22,13 +22,13 @@ class WattsUp(object):
                 port = '/dev/tty.usbserial-A1000wT3'
             elif system == 'Linux':
                 port = '/dev/ttyUSB0'
-
-        self.meter = True
-        self.interval = 1.0 if not interval else interval
         if os.path.isfile(port):
             self.s = serial.Serial(port, 115200 )
         else:
             self.meter = False
+
+        self.meter = True
+        self.interval = 1.0 if not interval else interval
         self.t = []
         self.power = []
         self.potential = []
@@ -36,6 +36,9 @@ class WattsUp(object):
         self.cpu = []
         self.memory = []
         self.thread = None
+
+    def logging(self):  
+        True if self.thread is not None else False
 
     def start(self):
         if self.thread is None:
@@ -119,80 +122,6 @@ class WattsUp(object):
         conn.commit()
         conn.close()
 
-    def average(self):
-        conn = sqlite3.connect('Databases/records.db')
-        cursor = conn.cursor()
-        try:
-            fd = open(self.machine_file, 'w')
-        except:
-            print 'Could not open file %s'
-            self.machine_file = None
-        for x in range(0, 15):
-            test_name = self.tests[x]
-            avg_elapsed_time = datetime.datetime.strptime('00:00:00.00', '%H:%M:%S.%f')
-            avg_total_watts = 0
-            avg_peak_watts = 0
-            avg_peak_current = 0
-            avg_peak_voltage = 0
-            cursor.execute("Select * FROM instances WHERE test_name=?", (test_name,))
-            if not cursor.rowcount == 0:
-                records = cursor.fetchall()
-                i = 0
-                for record in records:
-                    temp_time = datetime.datetime.strptime(record[4], '%H:%M:%S.%f')
-                    avg_elapsed_time += temp_time;
-                    avg_total_watts += float(record[5])
-                    avg_peak_watts += float(record[6])
-                    avg_peak_current += float(record[7])
-                    avg_peak_voltage += float(record[8])
-                    i += 1
-                avg_elapsed_time = (avg_elapsed_time / i)
-                avg_total_watts = (avg_total_watts / i)
-                avg_peak_watts = (avg_peak_watts / i)
-                avg_peak_current = (avg_peak_current / i)
-                avg_peak_voltage = (avg_peak_voltage / i)
-            cursor.execute("SELECT * FROM averages WHERE test_name=?", (test_name,))
-            if cursor.fetchall() is None:
-                cursor.execute("""UPDATE averages SET avg_time_elapsed=?, avg_total_wattage=?, avg_peak_wattage=?,
-				avg_peak_current=?, avg_peak_voltage=?  WHERE test_name=?""",
-				(avg_elapsed_time, avg_total_watts, avg_peak_watts, avg_peak_current, avg_peak_voltage, test_name))
-            else:
-                cursor.execute("""INSERT INTO averages (test_name, avg_time_elapsed, avg_total_wattage, avg_peak_wattage,
-				avg_peak_current, avg_peak_voltage) VALUES (?, ?, ?, ?, ?, ?, ?)""", 
-				(test_name, avg_elapsed_time, avg_total_watts, avg_peak_watts, avg_peak_current, avg_peak_voltage))
-            if self.machine_file:
-                fd.write('%s %s %3.1f %3.1f %5.3f %3.1f %3.1f\n' % (test_name, avg_elapsed_time, avg_total_watts, avg_peak_watts, avg_peak_current, avg_peak_voltage))
-        conn.commit()
-        conn.close()
-        try:
-            fd.close()
-        except:
-            pass
-
-    def transfer(self, passwrd):
-        try:
-            sftp = pysftp.Connection("harvey2.cc.binghamton.edu", username="rsmith23", password=passwrd)
-        except:
-            print "Failed to connect to web server."
-            self.webserver = None
-        if self.webserver:
-            info = uname()
-            sftp.cwd('public_html/Readings');
-            if not sftp.isdir(info[1]):
-                sftp.mkdir(info[1], mode=644)
-            sftp.cwd(info[1])
-            for i in range(0, 15):
-                test_file = 'Readings/' + self.tests[i]
-                try:
-                    sftp.put(test_file)
-                except:
-                    print 'Failed to transfer file'
-            machine_file = 'Readings/' + info[1]
-            sftp.put(machine_file)
-        try:
-            sftp.close()
-        except:
-            pass
 
     #Returns False if process is a zombie, otherwise True
     def procStatus(self, pid):
@@ -203,52 +132,3 @@ class WattsUp(object):
                     return False
                 else:
                     return True
-'''
-def main(args):
-    if not args.port:
-        system = uname()[0]
-        if system == 'Darwin':          # Mac OS X
-            args.port = '/dev/tty.usbserial-A1000wT3'
-        elif system == 'Linux':
-            args.port = '/dev/ttyUSB0'
-    if not os.path.exists(args.port):
-        if not args.sim:
-            print ''
-            print 'Serial port %s does not exist.' % args.port
-            print 'Please make sure FDTI drivers are installed'
-            print ' (http://www.ftdichip.com/Drivers/VCP.htm)'
-            print 'Default ports are /dev/ttyUSB0 for Linux'
-            print ' and /dev/tty.usbserial-A1000wT3 for Mac OS X'
-            exit()
-        else:
-            print ''
-            print 'File %s does not exist.' % args.port
-    if args.web:
-         passwd = getpass.getpass('Webserver password: ')
-    meter = WattsUp(args.port, args.interval)
-    if args.bench:
-        createDB();
-        meter.benchmark(args.outfile)
-        meter.record()
-        meter.average()
-    if args.web:
-         meter.transfer(passwd)
-    if args.internal:
-        meter.mode(INTERNAL_MODE)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Get data from Watts Up power meter.')
-    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='verbose')
-    parser.add_argument('-d', '--debug', dest='debug', action='store_true', help='debugging output')
-    parser.add_argument('-m', '--simulation-mode', dest='sim', action='store_true', help='simulate logging by reading serial data from disk with delay of sample interval between lines')
-    parser.add_argument('-i', '--internal-mode', dest='internal', action='store_true', help='Set meter to internal logging mode')
-    parser.add_argument('-l', '--log', dest='log', action='store_true', help='log data in real time')
-    parser.add_argument('-r', '--raw', dest='raw', action='store_true', help='output raw file')
-    parser.add_argument('-o', '--outfile', dest='outfile', default='log.out', help='Output file')
-    parser.add_argument('-s', '--sample-interval', dest='interval', default=1.0, type=float, help='Sample interval (default 1 s)')
-    parser.add_argument('-p', '--port', dest='port', default=None, help='USB serial port')
-    parser.add_argument('-w', '--weblog', dest='web', action='store_true', default=None, help='log data to webserver')
-    parser.add_argument('-b', '--benchmark', dest='bench', action='store_true', default=None, help='Run and record Dacapo')
-    args = parser.parse_args()
-    main(args)
-'''
