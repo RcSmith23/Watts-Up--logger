66f541b580ae56beb0cf1a16a491d9d3a15e762e
Ryan Smith <ryanc.smith@outlook.com>
2014-12-16 11:04:58 -0500

    Fixed break problems

--- wattsup.py
+++ wattsup.py
@@ -76,7 +76,7 @@ class WattsUp(object):
                     c = screen.getch()
                     if self.logfile:
                         fd.write('%s %d %3.1f %3.1f %5.3f %3.1f %3.1f\n' % (datetime.datetime.now(), n, W, V, A, C, M))
-            if x == 0 and n >= 30:
+            if args.bench == 0 and n >= 30:
                 break
             ps_status = [p.poll() for p in proc]
             if all([x is not None for x in ps_status]):
